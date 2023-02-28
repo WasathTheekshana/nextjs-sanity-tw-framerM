10c2b21358ad95af6b3f21a7420e31ccc8ed5719
WasathTheekshana <wasath.vt@gmail.com>
2023-02-28 20:56:53 +0530

    hero section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import { motion as m } from 'framer-motion'
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
 
@@ -5,8 +6,23 @@ type Props = {}
 
 function Header({ }: Props) {
     return (
-        <header>
-            <div>
+        <header className='p-5 sticky top-0 flex flex-row items-start justify-between max-w-7xl mx-auto z-20 xl:items-center '>
+            <m.div
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    // scale: 0.5,
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    // scale: 1,
+                }}
+                transition={{
+                    duration: 1.5,
+                    ease: 'easeInOut'
+                }}
+                className='flex flex-row items-center'>
                 {/* Social Icons */}
                 <SocialIcon
                     url='https://www.youtube.com/wasaththeekshana'
@@ -14,7 +30,45 @@ function Header({ }: Props) {
                     fgColor='gray'
                     bgColor='transparent'
                 />
-            </div>
+                <SocialIcon
+                    url='https://www.youtube.com/wasaththeekshana'
+                    target="_blank"
+                    fgColor='gray'
+                    bgColor='transparent'
+                />
+                <SocialIcon
+                    url='https://www.youtube.com/wasaththeekshana'
+                    target="_blank"
+                    fgColor='gray'
+                    bgColor='transparent'
+                />
+            </m.div>
+
+            <m.div
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    // scale: 0.5,
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    // scale: 1,
+                }}
+                transition={{
+                    duration: 1.5,
+                    ease: 'easeInOut'
+                }}
+                className='flex flex-row items-center cursor-pointer text-gray-300'>
+                <SocialIcon
+                    className='cursor-pointer'
+                    network='email'
+                    target="_blank"
+                    fgColor='gray'
+                    bgColor='transparent'
+                />
+                <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+            </m.div>
         </header>
     )
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link'
+import React from 'react'
+import { Typewriter, Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles'
+
+type Props = {}
+
+function Hero({ }: Props) {
+
+    const [text, count] = useTypewriter({
+        words: [
+            "It's me Wasath",
+            "The guy who loves coding",
+            "loves .tsx than .girlfriend"
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    })
+
+    return (
+        <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+            <BackgroundCircles />
+            <img
+                className='relative rounded-full h-32 w-32 mx-auto object-cover'
+                src="https://avatars.githubusercontent.com/u/91784445?v=4"
+                alt="Wasath Image"
+            />
+            <div className='z-20'>
+                <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[10px]'>Software Engineer</h2>
+                <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+                    <span className='mr-3'>{text}</span>
+                    <Cursor cursorColor='yello' />
+                </h1>
+
+                <div className='pt-5'>
+                    <Link href={"/about"}>
+                        <button className='heroButton'>About</button>
+                    </Link>
+                    <Link href={"/about"}>
+                        <button className='heroButton'>Experience</button>
+                    </Link>
+                    <Link href={"/about"}>
+                        <button className='heroButton'>Skills</button>
+                    </Link>
+                    <Link href={"/about"}>
+                        <button className='heroButton'>Projects</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
